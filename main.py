866f4829cd9a7666db87c629c8552a0d9c37ebf6
joanmanzanaro <42523299+joanmanzanaro@users.noreply.github.com>
2025-05-29 05:01:53 +0200

    Add main.py 1748487713176

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from main_window import MainWindow
 from PyQt6.QtWidgets import QApplication
 import sys
+from main_window import MainWindow
 
-if __name__ == "__main__":
+def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
